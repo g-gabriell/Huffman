670a8931717370784ac068433f7c5b17b4fd3f07
g-gabriell <gabrielgarcia8000@gmail.com>
2017-06-14 16:44:01 -0300

    prototipo da funcao cria arvorede huffman

--- include/arvore.c
+++ include/arvore.c
@@ -3,6 +3,23 @@
 
 #include "heap.h"
 
+
+cria_arvore_huffman(sub_arvore_t* folhas,int tam_folhas){
+    int n = tam_folhas;
+    fila_t* q = malloc(sizeof(fila_t));
+    inicializa_heap(n, )
+
+    for(i = 1;i <= n-1; i++){
+        sub_arvore_t* z = cria_sub_arvore()
+
+        z->f_esq = x = min_heapify()
+        z->f_dir = y = min
+        z.freq = x.freq + y.freq
+
+        enqueue(z, q)
+    }
+
+    }
 /*
 struct arvore{
     sub_arvore* raiz;
